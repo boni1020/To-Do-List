4ac36e437dcfc499214afb31539e80e34426bcfb
Boni <guaduckgua@gmail.com>
2024-08-20 15:34:01 +0800

    修正 新增後要清空input

--- app.js
+++ app.js
@@ -37,17 +37,16 @@ add.addEventListener("click", (e) => {
   checkButton.addEventListener("click", (e) => {
     let todoItem = e.target.parentElement;
     todoItem.classList.toggle("done");
-  })
+  });
 
   let trashButton = document.createElement("button");
   trashButton.classList.add("trash");
   trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
 
-  trashButton.addEventListener("click", e => {
+  trashButton.addEventListener("click", (e) => {
     let todoItem = e.target.parentElement;
 
     todoItem.addEventListener("animationend", () => {
-
       // remove from local storage
       let text = todoItem.children[0].innerText;
       let myListArray = JSON.parse(localStorage.getItem("list"));
@@ -56,14 +55,13 @@ add.addEventListener("click", (e) => {
           myListArray.splice(index, 1);
           localStorage.setItem("list", JSON.stringify(myListArray));
         }
-      })
+      });
 
       todoItem.remove();
-    })
+    });
 
     todoItem.style.animation = "scaleDown 0.3s forwards";
-  })
-
+  });
 
   todo.appendChild(checkButton);
   todo.appendChild(trashButton);
@@ -74,7 +72,7 @@ add.addEventListener("click", (e) => {
   let myTodo = {
     todoText: todoText,
     todoMonth: todoMonth,
-    todoDate: todoDate
+    todoDate: todoDate,
   };
 
   // store data into an array of objects
@@ -86,9 +84,12 @@ add.addEventListener("click", (e) => {
     myListArray.push(myTodo);
     localStorage.setItem("list", JSON.stringify(myListArray));
   }
-  form.children[0].value = ""; // CLEAR THE TEXT INPUT
+  // clear the text input
+  form.children[0].value = "";
+  form.children[1].value = "";
+  form.children[2].value = "";
   section.appendChild(todo);
-})
+});
 
 loadData();
 
@@ -96,8 +97,7 @@ function loadData() {
   let myList = localStorage.getItem("list");
   if (myList !== null) {
     let myListArray = JSON.parse(myList);
-    myListArray.forEach(item => {
-  
+    myListArray.forEach((item) => {
       // create a todo
       let todo = document.createElement("div");
       todo.classList.add("todo");
@@ -109,25 +109,24 @@ function loadData() {
       time.innerText = item.todoMonth + " / " + item.todoDate;
       todo.appendChild(text);
       todo.appendChild(time);
-  
+
       let checkButton = document.createElement("button");
       checkButton.classList.add("check");
       checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
-  
+
       checkButton.addEventListener("click", (e) => {
         let todoItem = e.target.parentElement;
         todoItem.classList.toggle("done");
-      })
-    
+      });
+
       let trashButton = document.createElement("button");
       trashButton.classList.add("trash");
       trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-    
-      trashButton.addEventListener("click", e => {
+
+      trashButton.addEventListener("click", (e) => {
         let todoItem = e.target.parentElement;
-    
+
         todoItem.addEventListener("animationend", () => {
-  
           // remove from local storage
           let text = todoItem.children[0].innerText;
           let myListArray = JSON.parse(localStorage.getItem("list"));
@@ -136,22 +135,21 @@ function loadData() {
               myListArray.splice(index, 1);
               localStorage.setItem("list", JSON.stringify(myListArray));
             }
-          })
-  
+          });
+
           todoItem.remove();
-        })
-    
+        });
+
         todoItem.style.animation = "scaleDown 0.3s forwards";
-      })
-  
+      });
+
       todo.appendChild(checkButton);
       todo.appendChild(trashButton);
-  
+
       section.appendChild(todo);
-    })
+    });
   }
 }
-
 
 function mergeTime(arr1, arr2) {
   let result = [];
@@ -213,4 +211,4 @@ sortButton.addEventListener("click", () => {
 
   // load data
   loadData();
-})
+});
